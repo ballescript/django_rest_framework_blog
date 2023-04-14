ff56feb415ebaeb1c2766936e82106b25b498207
ballescript <ballescript@gmail.com>
2023-04-14 13:49:08 -0700

    react

--- core/settings.py
+++ core/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     'blog',
     'blog_api',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -132,3 +134,7 @@ REST_FRAMEWORK = {
     'rest_framework.permissions.AllowAny',
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
