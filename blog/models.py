e1c5cd5f7e6eabed8eb697a1129b9ca66a99a8fa
ballescript <ballescript@gmail.com>
2023-04-17 17:46:24 -0700

    jwt

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils import timezone
+from django.conf import settings
 
 # Create your models here.
 class Category(models.Model):
@@ -25,7 +25,7 @@ class Post(models.Model):
     content = models.TextField()
     slug = models.TextField(max_length=250, unique_for_date='published')
     published = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
     status = models.CharField(max_length=10, choices=options, default='published')
     objects = models.Manager() # Default manager
     postobjects = PostObjects() # Custom manager
